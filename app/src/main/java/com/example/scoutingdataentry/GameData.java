88091e967a6466e3bde1882c6b1ce9b6dadc5023
ethanshedd <eshedd20@dtechhs.org>
2019-02-06 17:25:30 -0800

    readjust

---
+++ app/src/main/java/com/example/scoutingdataentry/GameData.java
@@ -0,0 +1,13 @@
+package com.example.scoutingdataentry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_data);
+    }
+}
